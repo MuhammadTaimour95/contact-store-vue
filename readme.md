4eb680fb17026bf01d9d171fa105bda84955e8f8
Muhammad Taimour <muhammad.taimour95@gmail.com>
2019-11-02 17:14:31 +0500

    Simple Contact store using Php laravel and vue js

---
+++ readme.md
@@ -0,0 +1,5 @@
+## About Contact store
+
+A simple contact store that uses php laravel as back end and Vue js as back end.
+The database technology used for this project is sqlite.
+Back end server is based on xamp.
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>Contact Store</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<div class='container'>
+<div id="app">
+<contacts></contacts>
+</div>
+</div> 
+    
+
+    <script src="/js/app.js"> </script>
+</body>
+</html>
